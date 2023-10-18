efba84cbff64900d18d68e528a6587529e866f95
Gayathri Pamuluru <gaya3.chinna@gmail.com>
2023-10-18 20:28:57 +0530

    3rd commit new function definition

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("NEW COMMIT")
+
+'''
+git remote add origin https://github.com/gaya3reddy/ai_project_test.git
+git branch -M main
+git push -u origin main
+'''
+
+def test(a,b):
+    return a+b
+sum_val = test(4,5)    
+print(sum_val)
